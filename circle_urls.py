06d0077f92332435886f675a7eb0cd582b8a2d55
Chris Karr <chris@audacious-software.com>
2024-02-26 16:56:55 -0600

    Update circle_urls.py

--- circle_urls.py
+++ circle_urls.py
@@ -1,12 +1,26 @@
-from django.conf.urls import include, url
+# pylint: disable=line-too-long, no-name-in-module
+
+import sys
+
 from django.contrib import admin
 from django.urls import path
 
 admin.autodiscover()
 admin.site.enable_nav_sidebar = False
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    url('^accounts/', include('django.contrib.auth.urls')),
-    url(r'^generative-ai/', include('simple_generative_ai.urls')),
-]
+if sys.version_info[0] > 2:
+    from django.urls import include, re_path
+
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        re_path('^accounts/', include('django.contrib.auth.urls')),
+        re_path(r'^generative-ai/', include('simple_generative_ai.urls')),
+    ]
+else:
+    from django.conf.urls import include, url
+
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        url('^accounts/', include('django.contrib.auth.urls')),
+        url(r'^generative-ai/', include('simple_generative_ai.urls')),
+    ]
