26e67b588882155ce550c5ceb2cd763f7b7c66af
Chris J. Karr <chris@audacious-software.com>
2026-02-12 14:58:00 +0000

    More robust validation checking

--- simple_generative_ai/openai_chat.py
+++ simple_generative_ai/openai_chat.py
@@ -75,25 +75,26 @@ def run_model(model_obj, prompt, user='openai_user', extras=None):
     return None
 
 def validate_model(model_obj):
-    parameters = model_obj.fetch_parameters()
-
     issues = []
 
-    parameters = model_obj.fetch_parameters()
+    try:
+        parameters = model_obj.fetch_parameters()
 
-    if parameters.get('openai_api_key', None) is None:
-        issues.append('Add valid "openai_api_key" or "azure_openai_api_key" parameter to model')
+        if parameters.get('openai_api_key', None) is None:
+            issues.append('Add valid "openai_api_key" or "azure_openai_api_key" parameter to model')
 
-    if parameters.get('openai_model', None) is None:
-        issues.append('Specify "openai_model" type parameter for model (see https://platform.openai.com/docs/models)')
+        if parameters.get('openai_model', None) is None:
+            issues.append('Specify "openai_model" type parameter for model (see https://platform.openai.com/docs/models)')
 
-    if parameters.get('azure_openai_api_key', None) is not None:
-        issues = []
+        if parameters.get('azure_openai_api_key', None) is not None:
+            issues = []
 
-        if parameters.get('azure_openai_endpoint', None) is None:
-            issues.append('Specify "azure_openai_endpoint" type parameter for model (see https://github.com/openai/openai-python/tree/main?tab=readme-ov-file#microsoft-azure-openai)')
+            if parameters.get('azure_openai_endpoint', None) is None:
+                issues.append('Specify "azure_openai_endpoint" type parameter for model (see https://github.com/openai/openai-python/tree/main?tab=readme-ov-file#microsoft-azure-openai)')
 
-        if parameters.get('azure_deployment_name', None) is None:
-            issues.append('Specify "azure_deployment_name" type parameter for model (see https://github.com/openai/openai-python/tree/main?tab=readme-ov-file#microsoft-azure-openai)')
+            if parameters.get('azure_deployment_name', None) is None:
+                issues.append('Specify "azure_deployment_name" type parameter for model (see https://github.com/openai/openai-python/tree/main?tab=readme-ov-file#microsoft-azure-openai)')
+    except: # pylint: disable=bare-except
+        issues.append('Unable to load or validate model "%s". Check that the parameters are formatted correctly.' % model_obj)
 
     return issues
